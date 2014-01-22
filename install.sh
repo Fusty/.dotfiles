adb382250c9008492e1f6d1c4d1b63eda1627b3c
Kooothor Sabre <kooothor@gmail.com>
2014-01-22 14:20:41 +0100

    modif install.sh, put all backup in backup dir

--- install.sh
+++ install.sh
@@ -2,19 +2,17 @@
 # File : install.sh
 # URL : http://github.com/kooothor/.dotfiles
 # Author : kooothor
-# Version : osef
 
 # make the links
 cd $HOME
 ln -s -i .dotfiles/.vim .vim
-ln -s -i .dotfiles/.vimrc .vimrc
+ln -s -i .dotfiles/.vim/vimrc .vimrc
 ln -s -i .dotfiles/.zsh .zsh
-ln -s -i .dotfiles/.zshrc .zshrc
+ln -s -i .dotfiles/.zsh/zshrc .zshrc
 ln -s -i .dotfiles/.tmux.conf .tmux.conf
 ln -s -i .dotfiles/.Xdefaults .Xdefaults
 ln -s -i .dotfiles/.conkyrc .conkyrc 
 ln -s -i .dotfiles/.mailcap .mailcap
 ln -s -i .dotfiles/.config/awesome ~/.config/awesome
 ln -s -i .dotfiles/.mpd .mpd
-ln -s -i .dotfiles/.mpdconf .mpdconf
 
